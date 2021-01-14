06b5440781a2e7a112026ae4ecc8e07d86efa921
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2021-01-14 23:47:45 +0530

    added widgets

--- Test-UI_en_US.ts
+++ Test-UI_en_US.ts
@@ -10,12 +10,47 @@
     </message>
     <message>
         <location filename="testui.ui" line="27"/>
-        <source>GroupBox</source>
+        <source>Main Window</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="testui.ui" line="42"/>
+        <location filename="testui.ui" line="61"/>
+        <source>This is Item 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testui.ui" line="66"/>
+        <source>Item 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testui.ui" line="71"/>
+        <source>Item 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testui.ui" line="76"/>
+        <source>Item 4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testui.ui" line="81"/>
+        <source>Item 5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testui.ui" line="86"/>
+        <source>Item 6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testui.ui" line="105"/>
         <source>Data Visualiser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testui.ui" line="110"/>
+        <source>Something Else</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
